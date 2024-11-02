5c257925d3c16ff2870774b679cf15c3229e20e7
Mat-94 <mathiasmahami@gmail.com>
2024-11-02 09:20:22 +0000

    adds new backup script

---
+++ mybackup2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Author: mathias
+#Description: simple backup script 
+# defining variables
+#
+BACKUP_SRC="/tmp"
+BACKUP_DST="/backups"
+BACKUP_DATE=$(date +%Y%m%d%H%M%S)
+BACKUP_FILENAME="backup_$BACKUP_DATE.tar.gz"
+
+
+mkdir -p "$BACKUP_DST/$BACKUP_DATE"
+
+# Archive the source directory
+
+tar -czf "$BACKUP_DST/$BACKUP_DATE_/$BACKUP_FILENAME" "$BACKUP_SRC"
+
+#verify the backup was created successfully
+
+if [ $? -eq 0 ]; then
+        echo "Backup successful: $BACKUP_FILENAME"
+else
+        echo "Backup failed" 
+fi
+
